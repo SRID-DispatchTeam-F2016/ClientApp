6ebd08cbf9c93d23f547c959419134bbad69b83c
U-DESKTOP-DHLIVVK\notanuwber <wangnan@cmu.edu>
2016-12-02 19:00:12 -0800

    add camera controller

---
+++ public/javascripts/controllers/cameraController.js
@@ -0,0 +1,7 @@
+angular.module('chatApp').controller('CameraController',
+    ['$window', '$scope', 'socket', 'rest',
+        function ($window, $scope, socket, rest) {
+            var vm = this;
+        }
+    ]
+);
