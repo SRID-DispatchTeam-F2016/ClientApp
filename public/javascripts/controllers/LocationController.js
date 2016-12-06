5a4be47eadee02fdee9e0a8328b2a53f8b6baf7e
U-DESKTOP-DHLIVVK\notanuwber <wangnan@cmu.edu>
2016-12-06 15:22:55 -0800

    add message after location is set

--- public/javascripts/controllers/LocationController.js
+++ public/javascripts/controllers/LocationController.js
@@ -3,8 +3,14 @@ angular.module('chatApp').controller('LocationController',
         function ($window, $scope, socket, rest, publicHistoryService, currentUserService) {
             var vm = this;
 
-            vm.click = function() {
-                alert(1);
+            vm.insert = function() {
+                var message = {
+                    username: "You",
+                    timestamp: Date.now(),
+                    content: "Position is Set"
+                };
+
+                publicHistoryService.insert(message);
             };
         }
     ]
